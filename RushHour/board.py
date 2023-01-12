4025f85ef8d8abc5581a45d4847afaf40f48bd13
Merijn Roelfsema <merijnroelfsema@MacBook-Air-van-Merijn.local>
2023-01-12 16:27:27 +0100

    Auto's worden nu goed op de grid geplaatst

--- RushHour/board.py
+++ RushHour/board.py
@@ -6,8 +6,10 @@ class Board:
     def __init__(self, dim: int, filename: str) -> None:
         self.dim = dim
         self.cars = []
-        self.grid = []
+        self.grid = np.full((dim, dim), "O")
         self.load_cars(f"gameboards/{filename}.csv")
+        self.add_cars_to_grid()
+        self.cars[0].get_car_spaces()
 
 
     def load_cars(self, file):
@@ -22,3 +24,13 @@ class Board:
                 self.cars.append(car)
 
                 line = f.readline()
+
+    def add_cars_to_grid(self):
+        for car in self.cars:
+            spaces = car.get_car_spaces()
+
+            for i in range(car.length):
+                space = spaces[i]
+                self.grid[space[0] - 1][space[1] - 1] = car.name
+
+        print(self.grid)
