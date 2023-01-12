281139369473f8486d34d14b0c8f5244c1c0596b
Merijn Roelfsema <merijnroelfsema@MacBook-Air-van-Merijn.local>
2023-01-12 16:41:07 +0100

    car.py verbeterd

--- RushHour/car.py
+++ RushHour/car.py
@@ -3,8 +3,10 @@ class Car:
     def __init__(self, name: str, orientation: str, col: int, row: int, length: int):
         self.name = name
         self.orientation = orientation
-        self.coordinates = [row, col]
-        self.length = length
+        self.row = int(row)
+        self.col = int(col)
+        self.coordinates = [self.row, self.col]
+        self.length = int(length)
 
 
     def __repr__(self):
@@ -13,6 +15,21 @@ class Car:
 
     def get_position(self):
         return self.coordinates
+
+
+    def get_car_spaces(self):
+        spaces = []
+
+        if self.orientation == "H":
+            for i in range(self.length):
+                space = [self.row, self.col + i]
+                spaces.append(space)
+        else:
+            for i in range(self.length):
+                space = [self.row + i, self.col]
+                spaces.append(space)
+
+        return spaces
 
 
     # def is_moveable(self):
