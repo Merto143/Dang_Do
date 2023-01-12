69830e4c07d91a037c95fe6cfc699ab7c26f1df3
Merijn Roelfsema <merijnroelfsema@sp-byods-145-109-7-242.wireless.uva.nl>
2023-01-12 12:03:42 +0100

    Load cars werkt nu in de Merijn file

--- RushHourMerijn.py
+++ RushHourMerijn.py
@@ -1,27 +1,39 @@
 from typing import Any, List
 from sys import argv
 
-j
-
 class Board:
+
     def __init__(self, dim: int, filename: str) -> None:
         self.dim = dim
+        self.cars = []
+        self.grid = []
         self.load_cars(f"gameboards/{filename}.csv")
 
+
     def load_cars(self, file):
-        cars = []
         with open(file) as f:
+
             line = f.readline()
-            line # Skipt eerste line
-            while line.strip() != "\n":
-                print(line)
+            line = f.readline()
+
+            while line != "":
                 line = line.split(",")
-                car = Car(line[0], line[1], line[2], line[3], line[4], line[5])
+                car = Car(line[0], line[1], line[2], line[3], line[4])
                 self.cars.append(car)
 
-        return cars
+                line = f.readline()
 
 
+class Car:
+
+    def __init__(self, name: str, orientation: str, col: int, row: int, length: int):
+        self.name = name
+        self.orientation = orientation
+        self.coordinates = [row, col]
+        self.length = length
+
+    def __repr__(self):
+        return self.name
 
 if __name__ == "__main__":
 
