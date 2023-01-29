1f7bb4646132daa9502d516192aa57945a64bfc5
CoenDeJong <coen@dejong.work>
2023-01-29 13:27:54 +0100

    Run nu python3 main.py RushHour 6x6_1 random

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from sys import argv
 from codefiles.classes.car import Car
 from codefiles.classes.board import Board
-# from codefiles.visualisations.visuals import *
+from codefiles.visualisations.visuals import *
 from codefiles.algorithms.algorithms import random_only_legal_moves_algorithm, random_all_moves_algorithm
 from codefiles.algorithms.algorithms import breadth_first
-from codefiles.algorithms.depth_first import DepthFirst
+# from codefiles.algorithms.depth_first import DepthFirst
 # =======
 from codefiles.algorithms.algorithms import breadth_first
 from codefiles.algorithms.algorithms import breadth_first
-from codefiles.algorithms.depth_first import DepthFirst
+# from codefiles.algorithms.depth_first import DepthFirst
 from codefiles.algorithms.breadth_first import BreadthFirst
 from codefiles.algorithms.breadth_first_random_states import BreadthFirstRandomStates
 
@@ -18,67 +18,65 @@ import time
 
 
 if __name__ == "__main__":
-    if len(argv) > 2:
-        print("Usage: python3 main.py [filename] OR python3 visualisations/visuals.py")
+    if len(argv) != 3:
+        print("Usage: python3 main.py [filename] [algorithm]")
         exit(1)
-    # elif len(argv) == 1:
-        # load_statistics()
-        # stat()
-    elif len(argv) == 2:
+    else:
         filename = argv[1]
+        algorithm = argv[2]
 
         if filename[9] == "2":
             dimension = 12
         else:
             dimension = int(filename[8])
     game = Board(dimension, filename)
-    df = DepthFirst(game)
 
-    df.run2()
+    if algorithm == "random":
+        print("Now random:")
+        game = Board(dimension, filename)
+        start = time.time()
+        iterations = random_only_legal_moves_algorithm(game)
+        end = time.time()
+        t = end - start
+        write_random(t, iterations, dimension)
 
-    # bf = breadth_first(game)
-    # bf.run()
-    # grid_visual(game)
-    # breadth_first(game)
+        print(f"It took {end - start} seconds to run the random algorithm")
 
-# >>>>>>> 9a71f9c505a2c9e0b5b735b33d08007da6afb0bc
-#     df = DepthFirst(game)
-#
-#     df.run()
-# =======
-#     grid_visual(game)
-#     # breadth_first(game)
-#
-# >>>>>>> 9a71f9c505a2c9e0b5b735b33d08007da6afb0bc
-    print("BreadthFirst:")
-    start = time.time()
-    Breadth_first = BreadthFirst(game)
-    Breadth_first.run()
-    end = time.time()
+    elif algorithm == "breadth":
+        print("Now breadth_first:")
+        game = Board(dimension, filename)
+        Breadth_first = BreadthFirst(game)
+        start = time.time()
+        Breadth_first.run()
+        end = time.time()
+        t = end - start
+        write_breadth(t, iterations, dimension)
 
-    print(f"It took {end - start} seconds to run the BreadthFirst algorithm")
-    print()
+    elif algorithm == "randombreadth":
+        game = Board(dimension, filename)
+        Breadth_first_random_states = BreadthFirstRandomStates(game)
+        start = time.time()
+        Breadth_first_random_states.run()
+        end = time.time()
+
+        print(f"It took {end - start} seconds to run the BreadthFirstRandomStates algorithm")
+        print()
+
+    elif algorithm == "depth":
+
+        df = DepthFirst(game)
+
+        df.run2()
+
+        bf = breadth_first(game)
+        bf.run()
+        breadth_first(game)
+
+        df = DepthFirst(game)
+
+        df.run()
 
 
-    game = Board(dimension, filename)
-
-    print("BreadthFirstRandomStates:")
-    start = time.time()
-    Breadth_first_random_states = BreadthFirstRandomStates(game)
-    Breadth_first_random_states.run()
-    end = time.time()
-
-    print(f"It took {end - start} seconds to run the BreadthFirstRandomStates algorithm")
-    print()
-
-
-    print("Now random:")
-    game = Board(dimension, filename)
-    start = time.time()
-    random_only_legal_moves_algorithm(game)
-    end = time.time()
-
-    print(f"It took {end - start} seconds to run the random algorithm")
 
     # nr = 10
     # results = []
